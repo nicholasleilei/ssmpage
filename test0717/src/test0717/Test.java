7598831c6968fd5b59648ad3d1795ebe0c7c5961
'jinxbinder' <1341385769@qq.com>
2019-07-17 11:54:04 +0800

    we

--- test0717/src/test0717/Test.java
+++ test0717/src/test0717/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("HelloWorld");
-System.out.println("bin");
+System.out.println("H4131elloWorld");
+System.out.println("bin3214");
 System.out.println("aaaaa11111");
 	}
 
