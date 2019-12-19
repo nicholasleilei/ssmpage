bb27fe7b5854e5bbf1f890e6e29e329fa6f7e1ae
雷蕾 <51227402+nicholasleilei@users.noreply.github.com>
2019-12-19 15:13:54 +0800

    d

--- test0717/src/test0717/Test.java
+++ test0717/src/test0717/Test.java
@@ -6,7 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 System.out.println("H4131elloWorld");
 System.out.println("bin3214");
-System.out.println("aaaaa111111");
+System.out.println("aaaaa11111sds1");
 	}
 
 }
